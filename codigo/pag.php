9358f2ac87d60e604763c729a25433abb5617668
matyrodriguez11 <94484395+matyrodriguez11@users.noreply.github.com>
2022-09-23 19:50:35 -0300

    Add files via upload

---
+++ codigo/pag.php
@@ -0,0 +1,7 @@
+$direccion # => localhost => 127.0.0.1 
+$id19615291_calendario
+$grupo4
+$Vacunacion4_
+    
+$enlace = mysqli_connect(localhost,grupo4,Vacunacion4_,id19615291_calendario);
+       
