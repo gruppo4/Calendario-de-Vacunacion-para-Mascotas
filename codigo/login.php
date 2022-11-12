dccdbb181e7ab8d24f93b667ffa0498e3b71c0ce
matyrodriguez11 <94484395+matyrodriguez11@users.noreply.github.com>
2022-11-11 23:46:12 -0300

    Add files via upload

---
+++ codigo/login.php
@@ -0,0 +1,21 @@
+<?php
+$dbhost = "localhost";
+$usuario = "id19615291_grupo4";
+$psw = "Vacunacion4_";
+$nombreBADA = "id19615291_calendario";
+$nombre = $_POST['nombre'];
+$contra = $_POST['psw'];
+
+$conn = mysqli_connect($dbhost, $usuario, $psw, $nombreBADA);
+$resultado = mysqli_query($conn, "Select * from dueño where nombre = '$nombre' AND psw = '$contra'");
+$filas = mysqli_num_rows($resultado);
+
+if ($filas>0){
+    header ("Location: https://vacunacionmascotas.000webhostapp.com/calendario.html");
+}else{
+    header ("Location: https://vacunacionmascotas.000webhostapp.com/index.html");
+}
+
+mysqli_free_result($resultado);
+mysqli_close($conn);
+?>
